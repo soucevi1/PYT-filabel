4655026eb31ceb6cc5f04548cc8e823cb263c736
Vit Soucek <soucevi1@fit.cvut.cz>
2018-10-08 11:34:04 +0200

    Added label conf parsing, getting filenames of modified files

--- filabel.py
+++ filabel.py
@@ -4,40 +4,53 @@ import requests
 import sys
 import json
 import pprint
+import fnmatch
 
 token = 'abc'
+
 
 def get_auth(f):
     """Get authentication token from config file"""
     config = configparser.ConfigParser()
     config.read_file(f)
-    return config['github']['token']
+    ret = config.get('github', 'token', fallback='Auth configuration not usable!')
+    return ret #config['github']['token']
+
 
 def token_auth(req):
     """Helper function for the session"""
     req.headers['Authorization'] = f'token {token}'
     return req
 
+
 def validate_repo_names(repos):
     """Tell whether repo names are valid"""
+    print('Validating repo names:')
     for r in repos:
         s = r.split('/')
         if (s[0] == r):
+            print(f'  Not valid: {r}.')
             return r
         if (s[0] == '') or (s[1] == ''):
+            print(f'  Not valid: {r}.')
             return r
         if (s[0].find('/') != -1) or (s[1].find('/') != -1):
-            return r 
+            print(f'  Not valid: {r}.')
+            return r
+        print(f'  {r}: OK')
     return True
+
 
 def create_session(config_auth):
     """Create session using the access token"""
+    print('Creating GitHub session.')
     global token
     token = get_auth(config_auth)
     session = requests.Session()
-    session.headers = {'User-Agent':'soucevi1'}
-    session.auth = token_auth 
+    session.headers = {'User-Agent': 'soucevi1'}
+    session.auth = token_auth
     return session
+
 
 def get_repo_prs(r, state, base, session):
     """
@@ -47,7 +60,8 @@ def get_repo_prs(r, state, base, session):
         base: base branch
         session: open and authenticated session
     """
-    payload = {'state':state, 'base':base}
+    print(f'Getting PRs of {r}')
+    payload = {'state': state, 'base': base}
     pulls = session.get(f'https://api.github.com/repos/{r}/pulls', params=payload)
     return pulls.json()
 
@@ -58,8 +72,53 @@ def get_pr_files(r, session, pull_num):
         session: open and authenticated session
         pull_num: number of the pull request
     """
+    print(f'Getting file json of {r}: PR {pull_num}')
     pull_files = session.get(f'https://api.github.com/repos/{r}/pulls/{pull_num}/files')
     return pull_files.json()
+
+
+def get_pr_filenames(fj):
+    """
+    Parse filenames from file json
+        fj: files json
+    """
+    print('Parsing filenames from the json file')
+    fns = []
+    for i in range(len(fj)):
+        fns.append(fj[i]['filename'])
+        #print(f"  filename: {fj[i]['filename']}")
+    return fns
+
+
+def get_labels(filenames, patterns):
+    """
+    Get labels to add to the PR
+        filenames: list of fns
+        paterns: dict "label:[pattern, p...]"
+    """
+
+
+
+def get_label_patterns(file):
+    """
+    Parse fn patterns from config file
+    """
+    config = configparser.ConfigParser()
+    config.read_file(file)
+    if config.has_section('labels') == False:
+        return {}
+    opts = config.options('labels')
+    ret = {}
+    for o in opts:
+        patterns = config.get('labels', o)
+        patts = patterns.split('\n')
+        if '' in patts:
+            patts.remove('')
+        ret[o] = patts
+    print(ret)
+    return ret
+    
+
 
 @click.command()
 @click.argument('REPOSLUGS', nargs=-1)
@@ -88,6 +147,13 @@ def main(config_auth, config_labels, reposlugs, state, delete_old, base):
         print(f'Reposlug {rep} not valid')
         sys.exit(1)
 
+    fpatterns = get_label_patterns(config_labels)
+    if fpatterns == {}:
+        print('Labels configuration not usable!')
+        sys.exit(1)
+
+    sys.exit(1)
+
     # Open a session
     session = create_session(config_auth)
 
@@ -95,14 +161,15 @@ def main(config_auth, config_labels, reposlugs, state, delete_old, base):
     for r in reposlugs:
         # Get all PRs of the current repo
         pulls_json = get_repo_prs(r, state, base, session)
-        # For each PR find its number and get info about the files
+        # For each PR find its number, current labels and get info about the files
         for n in range(len(pulls_json)):
             pull_num = pulls_json[n]['number']
+            labels_current = pulls_json[n]['labels']
             pull_files_json = get_pr_files(r, session, pull_num)
-            pprint.pprint(pull_files_json)
-        
+            pull_filenames = get_pr_filenames(pull_files_json)   
+            labels_to_add = get_labels(pull_filenames, fpatterns)                
 
-            # TODO: z file jsonu dostat nazvy souboru
+            # TODO: dopsat getlabels
 
 
     # pro kazdy repozitar:
